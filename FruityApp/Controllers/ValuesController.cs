e49ccede9f4e20f3dbc7c2d46a13bb03dbccc8e2
Luke Kellet <lukejameskellet@hotmail.com>
2023-10-25 15:37:10 +0100

    Add project files.

---
+++ FruityApp/Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.IO;
+using System.Text.Json.Serialization;
+using FruityApp.Controllers;
+
+
+namespace FruityApp.Controllers
+{
+    [Route("api/[controller]")]
+    [Consumes("application/json")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+        List<string> fruitList = new List<string>();
+        public ValuesController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public List<string> Get()
+        {
+            //List<string> fruitList = new List<string>();
+            string filepath = @"C:\Users\New-user\Desktop\SwaggerLogging\FruityApp\FruityApp\FruitFile.txt";
+            string text = System.IO.File.ReadAllText(filepath);
+            foreach(string line in
+            text.Split(","))
+            {
+                fruitList.Add(line);
+            }
+            _logger.LogError("Get performed sucessfully!!");
+            return fruitList;
+        }
+        [HttpPost]
+        public string? PostValue([FromBody] string? value)
+        {
+            //List<string> fruitList = new List<string>();
+            if (value != null && value != "")
+            {
+                string filepath = @"C:\Users\New-user\Desktop\SwaggerLogging\FruityApp\FruityApp\FruitFile.txt";
+                string text = System.IO.File.ReadAllText(filepath);
+                string totalText = (string)text + "," + value;
+                System.IO.File.WriteAllText(filepath, totalText);
+                foreach (string line in totalText.Split(","))
+                {
+                    fruitList.Add(line);
+                }
+            }
+            if (value != null && value != "")
+            {
+                _logger.LogInformation($"Added successfully: value : {value}");
+                return "Added successfully";
+            }
+            else
+            {
+                _logger.LogError("Error occured while adding value");
+                return "Not added - check formatting or other errors";
+            }
+        }
+    }
+}
---
+++ FruityApp/Program.cs
@@ -0,0 +1,41 @@
+namespace FruityApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                options.RoutePrefix = string.Empty;
+                options.DocumentTitle = "My application with Swagger";
+            });
+
+            app.Run();
+        }
+    }
+}
